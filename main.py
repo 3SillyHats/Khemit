d0637f9e529764006147f80be29dbe5785b3e851
Finn Stokes <finn.stokes@gmail.com>
2013-09-22 02:28:42 +0930

    Partially implemented lighting

--- main.py
+++ main.py
@@ -43,6 +43,12 @@ x_pos = 50.
 y_pos = 0.
 z_pos = 2.
 
+shader.use()
+li_loc = glGetUniformLocation(shader.program, "light_intensity")
+glUniform4f(li_loc, 1.0, 1.0, 1.0, 1.0)
+
+light_direction = numpy.array([1, 0, 0, 0], 'f')
+
 clock = pygame.time.Clock()
 while True:
     for event in pygame.event.get():
@@ -102,6 +108,10 @@ while True:
     #glEnable(GL_LIGHT0)
     #glLightfv(GL_LIGHT0, GL_POSITION, [0,100, 100])
     #glMaterialfv(GL_FRONT, GL_DIFFUSE, [1, 0, 1, 1.0]);
+
+    light_dir_camera_space = mv_matrix.dot(light_direction)
+    ld_loc = glGetUniformLocation(shader.program, "dir_to_light")
+    glUniform3fv(ld_loc, 1, numpy.array(light_dir_camera_space[:3]))
 
     for renderable in model.renderables():
         renderable.draw()
