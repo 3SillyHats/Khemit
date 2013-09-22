47f64c24a7640031c40cb6bce16ab7a877114150
Rory Stokes <helixwhiza@gmail.com>
2013-09-22 10:14:07 +0930

    fixing numpy dependance in camera

--- camera.py
+++ camera.py
@@ -7,16 +7,16 @@ class Camera(object):
 		self.setRot(x_rot, y_rot, z_rot)
 
 	def setPos(self,x,y,z=0):
-		self.pos = numpy.array([x,y,z])
+		self.pos = array([x,y,z])
 
 	def setRot(self,x,y,z=0):
 		y = min(max(y,-math.pi/2),math.pi/2)
 		x = x%(2*math.pi)
-		self.rot = numpy.array([x,y,z])
+		self.rot = array([x,y,z])
 		self.norm = [math.cos(x), math.sin(x)]
 
 	def move(self,x,y,z=0):
-		self.pos += numpy.array([x,y,z])
+		self.pos += array([x,y,z])
 
 	def rotate(self,x,y,z=0):
 		self.setRot(x+self.rot[0], y+self.rot[1], z+self.rot[2])
